7108bee551c8ef367eb48a3de4cfbbb1b7fc7afe
lastrodermus <lip_wiz@yahoo.com.br>
2014-08-25 14:41:02 -0300

    Alteração de teste

--- View/View.class.php
+++ View/View.class.php
@@ -1,25 +1,25 @@
 <?php
 
 class View {
+
     private $fPage;
     private $page;
-    
+
     public function __construct($pagina, $template = 'default') {
         $this->fPage = VIEW . $pagina . '.html';
-        if(!file_exists($this->fPage))
+        if (!file_exists($this->fPage))
             $this->fPage = VIEW . 'pagina_nao_encontrada.html';
         $temp = new Template($template);
         $this->page = $temp->getTemplate();
     }
-    
-    public function getPage($params = null){
+
+    public function getPage($params = null) {
         $contents = file_get_contents($this->fPage);
-        foreach($params as $chave => $valor){
+        foreach ($params as $chave => $valor) {
             $contents = str_replace('{{' . $chave . '}}', $valor, $contents);
         }
         $this->page = str_replace('{{PAGE}}', $contents, $this->page);
         return $this->page;
     }
-    
-    
+
 }
